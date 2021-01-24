c3319fbe543b4185547007799da6d74bc5363e64
mcereal <mpbcurtis@gmail.com>
2021-01-24 16:36:40 -0500

    input saving

--- src/components/EmployeeSearchForm/EmployeeSearchForm.tsx
+++ src/components/EmployeeSearchForm/EmployeeSearchForm.tsx
@@ -1,9 +1,36 @@
-import React from "react";
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
+import React, { ChangeEvent, useState } from "react";
 
 interface Props {}
 
+const DefaultInputState: Input = {
+  input: "",
+};
+
+interface Input {
+  input: string;
+}
+
 export const EmployeeSearchForm: React.FC<Props> = () => {
-  return <div></div>;
+  const [inputState, setinputState] = useState(DefaultInputState);
+  const handleInputChange = (
+    eventInput: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setinputState({ input: eventInput.target.value });
+  };
+  return (
+    <div>
+      <FormControl>
+        <TextField
+          value={inputState.input}
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Enter a name"
+        />
+      </FormControl>
+    </div>
+  );
 };
 
 export default EmployeeSearchForm;
--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ButtonAppBar } from "../../components/ButtonAppBar/ButtonAppBar";
 import EmployeeGrid from "../../components/EmployeeGrid/EmployeeGrid";
+import EmployeeSearchForm from "../../components/EmployeeSearchForm/EmployeeSearchForm";
 
 import "./SearchPage.css";
 
@@ -12,6 +13,7 @@ export const SearchPage: React.FC<Props> = () => {
   return (
     <div>
       <ButtonAppBar />
+      <EmployeeSearchForm />
       <EmployeeGrid />
     </div>
   );
