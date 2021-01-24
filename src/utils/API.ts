f30ebbb69a333e146f6e23f9de3e0db33aea1f83
mcereal <mpbcurtis@gmail.com>
2021-01-24 14:51:43 -0500

    giving one more result than needed

--- src/utils/API.ts
+++ src/utils/API.ts
@@ -5,7 +5,7 @@ const BASEURL = "https://randomuser.me/api/";
 const randomUserSearch = {
   search: async (searchRequests: number) => {
     const employees = [];
-    for (let i: number = 0; i <= searchRequests; i++) {
+    for (let i: number = 0; i < searchRequests; i++) {
       const request = await axios
         .get(BASEURL)
         .then((res) => res.data.results[0]);
