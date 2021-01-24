b1c3a6ec0df3ee0112495acee779622acc784320
mcereal <mpbcurtis@gmail.com>
2021-01-23 21:17:50 -0500

    not spreading info

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -19,30 +19,39 @@ export const SearchPage: React.FC<Props> = () => {
   const [employees, setEmployees] = useState(initialState);
 
   useEffect(() => {
-    API.search()
-      .then((res) => {
-        const { gender, email } = res;
-        const { first, last } = res.name;
-        const { large, medium, thumbnail } = res.picture;
+    for (let i = 0; i < 5; i++) {
+      API.search()
+        .then((res) => {
+          const { gender, email } = res;
+          const { first, last } = res.name;
+          const { large, medium, thumbnail } = res.picture;
 
-        setEmployees({
-          ...employees,
-          employeeList: [
-            {
-              gender,
-              name: { first, last },
-              email,
-              picture: {
-                large,
-                medium,
-                thumbnail,
+          setEmployees({
+            ...employees.employeeList,
+            employeeList: [
+              {
+                gender,
+                name: { first, last },
+                email,
+                picture: {
+                  large,
+                  medium,
+                  thumbnail,
+                },
               },
-            },
-          ],
-        });
-      })
-      .catch((err) => console.log(err));
-  }, [employees]);
+            ],
+          });
+          console.log(...employees.employeeList);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      {employees.employeeList.map((item) => (
+        <div>{item.name.first}</div>
+      ))}
+    </div>
+  );
 };
