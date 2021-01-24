69cdebb79ee935ab08e998a5434c67020f68590c
mcereal <mpbcurtis@gmail.com>
2021-01-24 15:18:42 -0500

    moved usefect from search page to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import { NoMatch } from "./pages/NoMatch/NoMatch";
+import { initialState } from "./pages/SearchPage/EmployeeState";
 import { SearchPage } from "./pages/SearchPage/SearchPage";
+import API from "./utils/API";
+import EmployeeContext from "./utils/EmployeeContext";
 
 const App: React.FC = () => {
+  const [employees, setEmployees] = useState(initialState);
+
+  useEffect(() => {
+    API.search(4)
+      .then((res) => {
+        res.forEach((employee) => {
+          const { gender, email } = employee;
+          const { first, last } = employee.name;
+          const { large, medium, thumbnail } = employee.picture;
+          const newEmployee = employees.employeeList;
+          newEmployee.push({
+            gender,
+            employeeName: { first, last },
+            email,
+            picture: {
+              large,
+              medium,
+              thumbnail,
+            },
+          });
+
+          setEmployees({
+            ...employees,
+            employeeList: newEmployee,
+          });
+        });
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <Router>
-      <div>
-        <Switch>
-          <Route exact path="/" component={SearchPage} />
-          <Route component={NoMatch} />
-        </Switch>
-      </div>
+      <EmployeeContext.Provider value={employees}>
+        <div>
+          <Switch>
+            <Route exact path="/" component={SearchPage} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </EmployeeContext.Provider>
     </Router>
   );
 };
--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,9 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { ButtonAppBar } from "../../components/ButtonAppBar/ButtonAppBar";
 import EmployeeGrid from "../../components/EmployeeGrid/EmployeeGrid";
-import API from "../../utils/API";
-import EmployeeContext from "../../utils/EmployeeContext";
-import { initialState } from "./EmployeeState";
+
 import "./SearchPage.css";
 
 interface Props {
@@ -11,42 +9,10 @@ interface Props {
 }
 
 export const SearchPage: React.FC<Props> = () => {
-  const [employees, setEmployees] = useState(initialState);
-
-  useEffect(() => {
-    API.search(4)
-      .then((res) => {
-        res.forEach((employee) => {
-          const { gender, email } = employee;
-          const { first, last } = employee.name;
-          const { large, medium, thumbnail } = employee.picture;
-          const newEmployee = employees.employeeList;
-          newEmployee.push({
-            gender,
-            employeeName: { first, last },
-            email,
-            picture: {
-              large,
-              medium,
-              thumbnail,
-            },
-          });
-
-          setEmployees({
-            ...employees,
-            employeeList: newEmployee,
-          });
-        });
-      })
-      .catch((err) => console.log(err));
-  }, []);
-
   return (
-    <EmployeeContext.Provider value={employees}>
-      <div>
-        <ButtonAppBar />
-        <EmployeeGrid />
-      </div>
-    </EmployeeContext.Provider>
+    <div>
+      <ButtonAppBar />
+      <EmployeeGrid />
+    </div>
   );
 };
