68c97896e39306a4ec317b6db274cbf43f248eb5
mcereal <mpbcurtis@gmail.com>
2021-01-24 19:22:36 -0500

    search kind of working

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,20 +1,54 @@
-import React from "react";
+import React, { ChangeEvent, useContext, useEffect, useState } from "react";
 import { ButtonAppBar } from "../../components/ButtonAppBar/ButtonAppBar";
 import EmployeeGrid from "../../components/EmployeeGrid/EmployeeGrid";
-import EmployeeSearchForm from "../../components/EmployeeSearchForm/EmployeeSearchForm";
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
 
 import "./SearchPage.css";
+import { initialState } from "./EmployeeState";
+import EmployeeContext from "../../utils/EmployeeContext";
 
 interface Props {
   text?: string;
 }
+const DefaultInputState: Input = {
+  input: "",
+};
+
+interface Input {
+  input: string;
+}
 
 export const SearchPage: React.FC<Props> = () => {
+  const [inputState, setinputState] = useState(DefaultInputState);
+  const [searchResults, setSearchResults] = useState(initialState);
+  const { employeeList } = useContext(EmployeeContext);
+  const handleInputChange = (
+    eventInput: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setinputState({ input: eventInput.target.value });
+  };
+
+  useEffect(() => {
+    const results = employeeList.filter((person) =>
+      person.name.first.toLowerCase().includes(inputState.input)
+    );
+    setSearchResults({ employeeList: results });
+  }, [inputState]);
   return (
     <div>
-      <ButtonAppBar />
-      <EmployeeSearchForm />
-      <EmployeeGrid />
+      <EmployeeContext.Provider value={searchResults}>
+        <ButtonAppBar />
+        <FormControl>
+          <TextField
+            value={inputState.input}
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Enter a name"
+          />
+        </FormControl>
+        <EmployeeGrid />
+      </EmployeeContext.Provider>
     </div>
   );
 };
--- src/utils/EmployeeSearchContext.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { initialState } from "../pages/SearchPage/EmployeeState";
-
-const EmployeeContext = React.createContext(initialState);
-
-export default EmployeeContext;
